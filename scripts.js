4487289316f1a54067a81605355c069f91e9b324
Todd Watner <toddwatner@gmail.com>
2017-09-04 14:12:49 -0600

    attempt to create card

--- scripts.js
+++ scripts.js
@@ -1,17 +1,43 @@
+$('.idea-input-form').on('submit', function(e) {
+  e.preventDefault();
+  getIdeaValues();
+});
+
 function Idea(title, body, quality = [0], id = Date.now()){
   this.title = title;
   this.body = body;
   this.quality = quality;
   this.id = id;
-}
+};
 
 function getIdeaValues() {
   var ideaTitle = $('.title-input').val();
   var ideaBody = $('.body-input').val();
   var newIdea = new Idea(ideaTitle, ideaBody);
-}
+  console.log(newIdea);
+  return newIdea;
+};
 
-$('.idea-input-form').on('submit', function(e) {
-  e.preventDefault();
-  getIdeaValues();
-});
+function createIdeaCard() {
+  $('.idea-holder').prepend(fillIdeaCard(getIdeaValues()));
+};
+
+function fillIdeaCard (newIdea) {
+  var newTitle = newIdea.title;
+  var newBody = newIdea.body;
+  var newId = newIdea.id;
+  var newQuality = newIdea.quality;
+  return (`<article id="${newId}" class="idea-card">
+            <div>
+              <h3 class="title">${newTitle}</h3>
+              <button class="delete-button"></button>
+            </div>
+            <p class="idea-body">${newBody}</p>
+            <div class="quality-container">
+              <button class="upvote"></button>
+              <button class="downvote"></button>
+              <h5>${newQuality}</h5>
+              <p class="quality">swill</p>
+            </div>
+          </article>`)
+};
