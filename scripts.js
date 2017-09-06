ff4c5da00c2724ea0be6124cc0c10c6a06d97a4d
Todd Watner <toddwatner@gmail.com>
2017-09-06 16:35:51 -0600

    local storage save

--- scripts.js
+++ scripts.js
@@ -1,9 +1,12 @@
+
+
 $('.idea-input-form').on('submit', function(e) {
   e.preventDefault();
-  createIdeaCard()
+  createIdeaCard();
+  
 });
 
-function Idea(title, body, quality = [0], id = Date.now()){
+function Idea(title, body, quality = 0, id = Date.now()){
   this.title = title;
   this.body = body;
   this.quality = quality;
@@ -18,17 +21,37 @@ function getIdeaValues() {
 };
 
 function createIdeaCard() {
-  $('.idea-holder').prepend(fillIdeaCard(getIdeaValues()));
+  var idea = getIdeaValues()
+  $('.idea-holder').prepend(fillIdeaCard(idea));
+  var allIdeas = getFromLocalStorage()
+  allIdeas.push(idea)
+  saveToLocalStorage(allIdeas)
 };
 
 function deleteIdea(id) {
   $('#' + id).remove();
 }
 
-function getQuality (qualityNumber) {
+function getQuality(qualityNumber) {
   var qualityArray = ['swill', 'plausible', 'genius'];
     return qualityArray[qualityNumber];
 }
+
+function upvote(id){
+
+}
+
+function saveToLocalStorage(allTheIdeas){
+  localStorage.setItem("ideaArray", JSON.stringify(allTheIdeas))
+}
+
+function getFromLocalStorage(){
+  if (JSON.parse(localStorage.getItem("ideaArray"))) {
+    return JSON.parse(localStorage.getItem("ideaArray"))
+  } else {
+    return []
+  }
+};
 
 function fillIdeaCard (newIdea) {
   var newTitle = newIdea.title;
@@ -43,7 +66,7 @@ function fillIdeaCard (newIdea) {
             </div>
             <p class="idea-body">${newBody}</p>
             <div class="quality-container">
-              <button class="upvote"></button>
+              <button class="upvote" onclick="upvote(${newId})"></button>
               <button class="downvote"></button>
               <h5>quality:</h5>
               <p class="quality">${newQuality}</p>
