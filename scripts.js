a73edaf99a4adb4c3236ee9391e9db6b25454966
Todd Watner <toddwatner@gmail.com>
2017-09-07 10:49:39 -0600

    quality buttons, input borders, top padding

--- scripts.js
+++ scripts.js
@@ -42,12 +42,15 @@ function fillIdeaCard (newIdea) {
               <button class="delete-button" onclick="deleteIdea(${newId})"></button>
             </div>
             <p class="idea-body">${newBody}</p>
-            <div class="quality-container">
+            <div class="quality-button-container">
               <button class="upvote"></button>
               <button class="downvote"></button>
+            </div>
+            <div class="quality-string-container">
               <h5>quality:</h5>
               <p class="quality">${newQuality}</p>
             </div>
+            <hr>
           </article>
-          <hr>`)
+          `)
 };
